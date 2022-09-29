c242e3bb940d238398c2514d107d04f43629fb47
tina bayat <tinaa.bayat@gmail.com>
2022-09-29 18:07:28 +0200

    make code shorter and use mymodule

--- celebrity-game.py
+++ celebrity-game.py
@@ -1,37 +1,11 @@
 import getpass
 import time
 import argparse
+from mymodule import take_guess , take_words_list, game_turn
 
 
 parser = argparse.ArgumentParser(description="It is a word game named celebrity. In the celebrity game, you try to guess the words by using words and sentences that describe them. It sounds simple: To start the celebrity game, divide into two teams. Ask the members of each group to enter famous and funny words that they think everyone (ormost people) knows, without the other members of their group and the other group seeing (maximum five words for each group in the computer version);for example, Tolstoy or Anna Karenina. Explain to your group that only these words are used in this round of the game; Therefore, it is necessary to pay attention when the opposing team is playing; Because they may need to guess that word in their turn. Each round is done the same way. The first person on the team takes a word from the list and tries to convey it to his teammates, and when his teammates guess the name, the next person on the team gets up and does the same for his team. Keep doing this until time runs out, and then the teams switch places. The game continues in this way until the entire list is empty.")
 
-
-def take_guess(count,group):
-    try:
-        guess = (input(f"{count}> group {group} > please guess the word: "))
-    except ValueError:
-        return take_guess(count, group)
-    return guess
-
-def take_words_list(number_of_group):
-    words_list = []
-    for group in range(1 , number_of_group +1):
-        item = getpass.getpass(prompt = f"group {group} please enter your words(can be name of celebrity , name of books ,name of authors , even swear word for fun):")
-        words_list = words_list + item.split()
-    return words_list
-
-
-def game_turn(actual_word, round_number, group_name, new_list):
-    guess = take_guess(round_number, group_name)
-
-    won = False
-    if guess != actual_word :
-        print("your guess is incorrect")
-    else:
-        new_list.remove(actual_word)
-        won = True
-
-    return won,new_list
 
 def word_game(number_of_group):
     
@@ -64,18 +38,18 @@ def word_game(number_of_group):
                 groups_win[group]=number_of_true
             
 
-    return groups_win , number_of_true
+    return groups_win 
 
 def main():
     args = parser.parse_args()
 
     option = 2
-    final_result , correct = word_game(option)
+    final_result = word_game(option)
     if 2 in final_result and final_result[1] == final_result[2]:
         print("The two groups were tied")
     else:
         winner = max(final_result, key=final_result.get)
-        print(f"group {winner} with {correct} correct guess win!")
+        print(f"group {winner} with {final_result[winner]} correct guess win!")
     
 main()
     
